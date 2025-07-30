82d08390d57ff923b2d74ceb64e258a9a3a1aeb2
Lavaill <hugo.lavaill@gmail.com>
2025-07-30 13:55:43 +0200

    Draw call for the goal

--- goal.gd
+++ goal.gd
@@ -1,1 +1,13 @@
 extends Node2D
+
+var shape : Vector2 = Vector2(0,0)
+var goalPosition : Vector2 = Vector2(0,0)
+
+func _ready() -> void:
+	# Warning: Will break if the shape is changed.
+	shape = %GoalCollisionShape.shape.size
+	goalPosition = Vector2(%GoalCollisionShape.position.x-(%GoalCollisionShape.shape.size.x/2),%GoalCollisionShape.position.y-%GoalCollisionShape.shape.size.y/2)
+
+func _draw():
+	#Use Transform2D to rotate the Rect2 if needed.
+	draw_rect(Rect2(goalPosition, shape),Color.WHITE_SMOKE, true)
